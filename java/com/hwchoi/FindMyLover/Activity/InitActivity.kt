8c1412e66607f590c8c3dabd44aaad223ea3e91b
ChoiHyungWuk <93chw@kakao.com>
2023-06-14 19:37:09 +0900

    [update][add] add auto login logic

--- java/com/hwchoi/FindMyLover/Activity/InitActivity.kt
+++ java/com/hwchoi/FindMyLover/Activity/InitActivity.kt
@@ -1,14 +1,38 @@
 package com.hwchoi.FindMyLover.Activity
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.PersistableBundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hwchoi.FindMyLover.R
 import com.hwchoi.FindMyLover.Utils.mIntroDelay
 
 class InitActivity : AppCompatActivity() {
+
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+            } else -> {
+            // No location access granted.
+        }
+        }
+    }
+
+    lateinit var auth: FirebaseAuth //파이어베이스 인증 객체
 
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
@@ -20,10 +44,47 @@ class InitActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        auth = Firebase.auth
 
         Handler().postDelayed({
-            GoToLogin()
+            autoLogin()
         }, mIntroDelay.toLong())
+
+
+//        when {
+//            ContextCompat.checkSelfPermission(
+//                CONTEXT,
+//                Manifest.permission.
+//            ) == PackageManager.PERMISSION_GRANTED -> {
+//                // You can use the API that requires the permission.
+//            }
+//            shouldShowRequestPermissionRationale(...) -> {
+//            // In an educational UI, explain to the user why your app requires this
+//            // permission for a specific feature to behave as expected, and what
+//            // features are disabled if it's declined. In this UI, include a
+//            // "cancel" or "no thanks" button that lets the user continue
+//            // using your app without granting the permission.
+//            showInContextUI(...)
+//        }
+//            else -> {
+//                // You can directly ask for the permission.
+//                // The registered ActivityResultCallback gets the result of this request.
+//                requestPermissionLauncher.launch(
+//                    Manifest.permission.REQUESTED_PERMISSION)
+//            }
+//        }
+
+
+
+// ...
+
+// Before you perform the actual permission request, check whether your app
+// already has the permissions, and whether your app needs to show a permission
+// rationale dialog. For more details, see Request permissions.
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+
     }
 
     private fun GoToLogin() {
@@ -36,7 +97,45 @@ class InitActivity : AppCompatActivity() {
         finish()
     }
 
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+            }
+        }
+
     override fun onBackPressed() {
+    }
+
+    private fun autoLogin() {
+        val sharedPref = getSharedPreferences(
+            getString(R.string.auto_login_key), Context.MODE_PRIVATE
+        )
+
+        if (sharedPref.getBoolean("autoCheck", false)) {
+            auth.signInWithEmailAndPassword(
+                sharedPref.getString("id", null).toString(),
+                sharedPref.getString("passwd", null).toString()
+            )
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        GoToMain()
+                    } else {
+                        GoToLogin()
+                    }
+                }
+        } else {
+            GoToLogin()
+        }
     }
 
 }
--- java/com/hwchoi/FindMyLover/Fragment/LoginFragment.kt
+++ java/com/hwchoi/FindMyLover/Fragment/LoginFragment.kt
@@ -1,10 +1,14 @@
 package com.hwchoi.FindMyLover.Fragment
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -19,10 +23,21 @@ import com.hwchoi.FindMyLover.Utils.signUpFormatChecker
 
 class LoginFragment : Fragment() {
 
+    lateinit var activity: Activity
     lateinit var auth: FirebaseAuth //파이어베이스 인증 객체
 
     lateinit var mInputId: String
     lateinit var mInputPassword: String
+
+    lateinit var mLoginBtn: Button
+    lateinit var mSignUpBtn: Button
+
+    lateinit var mIdEditText: EditText
+    lateinit var mPasswordEditText: EditText
+
+    lateinit var mAutoLoginCheckBox: CheckBox
+
+    lateinit var sharedPref: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,13 +46,17 @@ class LoginFragment : Fragment() {
     ): View? {
         val view: View = inflater.inflate(R.layout.login_layout, container, false)
 
-        val mLoginBtn: Button = view.findViewById(R.id.go_main_btn)
-        val mSignUpBtn: Button = view.findViewById(R.id.sign_up_btn)
+        mLoginBtn = view.findViewById<Button>(R.id.go_main_btn)
+        mSignUpBtn = view.findViewById<Button>(R.id.sign_up_btn)
 
-        val mIdEditText = view.findViewById<EditText>(R.id.email_edit_text)
-        val mPasswordEditText = view.findViewById<EditText>(R.id.passwd_edit_text)
+        mIdEditText = view.findViewById<EditText>(R.id.email_edit_text)
+        mPasswordEditText = view.findViewById<EditText>(R.id.passwd_edit_text)
 
-        val activity = activity as LoginActivity
+        mAutoLoginCheckBox = view.findViewById<CheckBox>(R.id.auto_login)
+
+        sharedPref = activity.getSharedPreferences(
+            getString(R.string.auto_login_key), Context.MODE_PRIVATE
+        )
 
         mLoginBtn.setOnClickListener {
             mInputId = mIdEditText.text.toString()
@@ -57,8 +76,17 @@ class LoginFragment : Fragment() {
             } else {
                 auth.signInWithEmailAndPassword(mInputId, mInputPassword)
                     .addOnCompleteListener(activity) { task ->
+                        mPasswordEditText.text.clear()
                         if (task.isSuccessful) {
-                            activity.goToMain()
+                            if (mAutoLoginCheckBox.isChecked) {
+                                with(sharedPref.edit()) {
+                                    putBoolean("autoCheck", true)
+                                    putString("id", mInputId)
+                                    putString("passwd", mInputPassword)
+                                    apply()
+                                }
+                            }
+                            (activity as LoginActivity).goToMain()
                         } else {
 
                         }
@@ -66,15 +94,21 @@ class LoginFragment : Fragment() {
             }
         }
         mSignUpBtn.setOnClickListener {
-            activity.showSignUpFragment()
+            (activity as LoginActivity).showSignUpFragment()
         }
 
         return view
     }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = context as LoginActivity
+    }
+
     override fun onResume() {
         super.onResume()
         auth = Firebase.auth
+
     }
 
 }
